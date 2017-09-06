327a9b4f31dcb09c8cf2138b7cde3dfa951471c8
Ken Youens-Clark <kyclark@gmail.com>
2017-09-06 18:06:53 -0500

    tsv files need more handling

--- stampede/scripts/collapse.py
+++ stampede/scripts/collapse.py
@@ -7,6 +7,7 @@
 import argparse
 import re
 import os
+import csv
 
 # --------------------------------------------------
 def main():
@@ -38,31 +39,73 @@ def main():
         exit(1)
 
     for i, fasta in enumerate(fasta_files):
-        print('{}: {}'.format(i + 1, fasta))
+        print('{:4}: {}'.format(i + 1, fasta))
         basename, ext = os.path.splitext(os.path.basename(fasta))
         splits = sorted([f for f in split_files if re.match(basename, f)])
 
         for file_type in ['.tsv', '.sum']:
-            files = [f for f in splits if f.endswith(file_type)]
+            files = [os.path.join(reports_dir, f)
+                     for f in splits if f.endswith(file_type)]
             if len(files) < 1:
                 print('WARNING: No files ending with "{}" for "{}"',
                       file_type, fasta)
             else:
                 out_path = os.path.join(out_dir, fasta + file_type)
-                print('  Writing to "{}"'.format(out_path))
+                print('      Writing to "{}"'.format(out_path))
                 out_fh = open(out_path, 'w')
-                for fnum, file in enumerate(files):
-                    print('  {}: {}'.format(fnum + 1, file))
-                    in_fh = open(os.path.join(reports_dir, file))
-                    hdr = in_fh.readline()
-                    if fnum == 0:
-                        out_fh.write(hdr)
 
-                    for line in in_fh:
-                        out_fh.write(line)
-
+                if file_type == '.tsv':
+                    write_tsv(files, out_fh)
+                else:
+                    write_sum(files, out_fh)
 
     print('Done, see output dir "{}"'.format(out_dir))
+
+# --------------------------------------------------
+def write_tsv(files, out_fh):
+    """collapse tsv files"""
+    tax = dict()
+    num_flds = ['numReads', 'numUniqueReads']
+
+    for fnum, file in enumerate(files):
+        print('    {:4}: {}'.format(fnum + 1, os.path.basename(file)))
+        with open(file) as csvfile:
+            reader = csv.DictReader(csvfile, delimiter='\t')
+            for row in reader:
+                tax_id = row['taxID']
+                if not tax_id in tax:
+                    tax[tax_id] = dict()
+                    for fld in row.keys():
+                        tax[tax_id][fld] = int(row[fld]) if fld in num_flds else row[fld]
+                else:
+                    for fld in num_flds:
+                        tax[tax_id][fld] += int(row[fld])
+
+    # Write the headers
+    flds = ['name', 'taxID', 'taxRank', 'genomeSize'] + num_flds + ['abundance']
+    out_fh.write("\t".join(flds) + '\n')
+
+    total_reads = sum([tax[s]['numReads'] for s in tax])
+
+    for tax_id in sorted(tax.keys(), key=int):
+        species = tax[tax_id]
+
+        species['abundance'] = round(species['numReads'] / total_reads, 2)
+
+        out_fh.write('\t'.join([str(species[f]) for f in flds]) + '\n')
+
+# --------------------------------------------------
+def write_sum(files, out_fh):
+    """collapse sum files"""
+    for fnum, file in enumerate(files):
+        print('    {:4}: {}'.format(fnum + 1, os.path.basename(file)))
+        in_fh = open(file, 'r')
+        hdr = in_fh.readline()
+        if fnum == 0:
+            out_fh.write(hdr)
+
+        for line in in_fh:
+            out_fh.write(line)
 
 # --------------------------------------------------
 def get_args():
