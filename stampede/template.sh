c7e71a1fcaed82fe53f94579d28a3984b2e72b2e
Ken Youens-Clark <kyclark@email.arizona.edu>
2018-07-02 11:04:49 -0500

    quotes

--- stampede/template.sh
+++ stampede/template.sh
@@ -7,4 +7,4 @@ echo "INDEX            \"${INDEX}\""
 echo "EXCLUDE_TAXIDS   \"${EXCLUDE_TAXIDS}\""
 echo "FIGURE_TITLE     \"${FIGURE_TITLE}\""
 
-sh run.sh ${QUERY} ${READS_ARE_PAIRED} ${FORMAT} ${INDEX} ${EXCLUDE_TAXIDS} ${FIGURE_TITLE}
+sh run.sh "${QUERY}" "${READS_ARE_PAIRED}" "${FORMAT}" "${INDEX}" "${EXCLUDE_TAXIDS}" "${FIGURE_TITLE}"
