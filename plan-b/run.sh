68ecd90f00d709d5f7cb997c0b93fe7029f848e2
Matt Bomhoff <mbomhoff@email.arizona.edu>
2019-11-12 12:06:47 -0700

    Add plan-b support

---
+++ plan-b/run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -u
+
+echo $PWD
+CWD=$(dirname "$0")
+IMG="$CWD/centrifuge-1.0.4.img"
+OUT_DIR="$PWD/centrifuge-out"
+INDEX_DIR="/data/centrifuge-indexes"
+RUN_CENTRIFUGE="singularity exec $IMG run_centrifuge.py"
+
+#
+# Show HELP if no arguments
+#
+[[ $# -eq 0 ]] && $RUN_CENTRIFUGE -h
+
+#
+# Verify existence of INDEX_DIR, chosen INDEX
+#
+if [[ ! -d "$INDEX_DIR" ]]; then
+    echo "Cannot find INDEX_DIR \"$INDEX_DIR\""
+    exit 1
+fi
+
+$RUN_CENTRIFUGE -I "$INDEX_DIR" "$@"
