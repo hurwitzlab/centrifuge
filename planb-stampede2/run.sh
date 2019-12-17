53f42ef352f20c10e8674a998afaf80356526f29
Matt Bomhoff <mbomhoff@email.arizona.edu>
2019-12-17 15:29:50 -0700

    Pass slurm params on command-line

--- planb-stampede2/run.sh
+++ planb-stampede2/run.sh
@@ -1,22 +1,11 @@
 #!/bin/bash
 
-#SBATCH -A iPlant-Collabs
-#SBATCH -N 1
-#SBATCH -n 1
-#SBATCH -t 24:00:00
-#SBATCH -p skx-normal
-
-if [[ -z "$JOBID" ]]; then
-    echo "Error: missing JOBID"
-    exit 1
-fi
-
 module load tacc-singularity
 
-IMG="/work/04848/mbomhoff/stampede2/plan-b/$JOBID/plan-b_stampede2/centrifuge-1.0.4.img"
-OUT_DIR="/work/04848/mbomhoff/stampede2/plan-b/$JOBID/data/centrifuge-out"
+IMG="$PLANB_JOBPATH/planb-stampede2/centrifuge-1.0.4.img"
+OUT_DIR="$PLANB_JOBPATH/data/centrifuge-out"
 INDEX_DIR="/work/05066/imicrobe/iplantc.org/data/centrifuge-indexes"
-RUN_CENTRIFUGE="singularity exec -B /work:/work $IMG run_centrifuge.py"
+RUN_CENTRIFUGE="singularity exec -B $PLANB_JOBPATH:$PLANB_JOBPATH -B /work:/work $IMG run_centrifuge.py"
 
 #
 # Show HELP if no arguments
